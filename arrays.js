66659f2c60d612fd8abf8f2e57db9ebee3315912
Natalie Rodriguez <codinggeographer@gmail.com>
2023-04-20 04:01:57 -0600

    call pop method on string array; call length method on number array; check index of array element;

--- arrays.js
+++ arrays.js
@@ -1,3 +1,20 @@
 var stringArray = ["string1", "string2", "string3", "string4"]
 var numberArray = [1, 2, 3, 4]
 var booleanArray = [true, false, true, false]
+
+// calls .pop() method on stringArray
+stringArray.pop();
+// should remove last element from the array
+console.log(stringArray);
+// should print out all except "string4"
+
+// calls .length() on numberArray
+console.log(numberArray.length);
+// will give the length / count of how 
+// many elements are inside array
+// will print out 4
+
+console.log(booleanArray[1]);
+// should return "false" beause the 
+// element with index 1 in the booleansArray
+// is false
